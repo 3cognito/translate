0e877b97281f9c7fea1e3649948d8b2f0aadc303
Tommy <tommyubongabasijohn@gmail.com>
2023-01-19 07:27:06 +0100

    added translate function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import testFxn from "./tools";
+import { testFxn, translate } from "./tools";
 
 dotenv.config();
 
@@ -11,6 +11,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
+// app.get('/translate', )
+
+app.listen(port, async () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  const res = await testFxn("./Tommy.pdf");
+  const test: any = await translate(res, "french");
+  console.log(test.data.choices[0].text);
 });
--- src/tools.ts
+++ src/tools.ts
@@ -1,14 +1,40 @@
 import * as fs from "fs";
 import pdf, { Result } from "pdf-parse";
+import { Configuration, OpenAIApi } from "openai";
+import dotenv from "dotenv";
+dotenv.config();
 
-const testFxn = async (pathToFile: string): Promise<Result | undefined> => {
+const config = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+const testFxn = async (pathToFile: string): Promise<string | undefined> => {
   try {
     const pdfBuffer: Buffer = fs.readFileSync(pathToFile);
     const data: Result = await pdf(pdfBuffer);
-    return data;
+    return data.text;
   } catch (err) {
     console.log(err);
   }
 };
 
-export default testFxn;
+const translate = async (
+  text: string | undefined,
+  lang: string
+): Promise<String | undefined> => {
+  try {
+    const prompt: string = `Translate ${text} to ${lang}`;
+
+    const result: any = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt,
+    });
+    console.log(result.data.choices[0].text);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { testFxn, translate };
